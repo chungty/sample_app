65567fa0d14f6cdeb7edd16b6fa889040eb9b64d
chungty <chungty@gmail.com>
2013-01-18 01:09:43 -0800

    Finish sign in fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,15 +10,15 @@ module ApplicationHelper
 		end
 	end
 	
-	RSpec::Matchers.define :have_error_message do |message|
-		match do |page|
-			page.should have_selector('div.alert.alert-error', text: message)
-		end
-	end
+	# RSpec::Matchers.define :have_error_message do |message|
+	# 	match do |page|
+	# 		page.should have_selector('div.alert.alert-error', text: message)
+	# 	end
+	# end
 
-	def valid_signin(user)
-		fill_in "Email",	with: user.email
-		fill_in "Password",	with: user.password
-		click_button "Sign in"
-	end
+	# def valid_signin(user)
+	# 	fill_in "Email",	with: user.email
+	# 	fill_in "Password",	with: user.password
+	# 	click_button "Sign in"
+	# end
 end
